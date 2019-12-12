35b56adf013ebcfc356bde0b8c2183face921c17
Brian Wyld <brian.wyld@wyres.fr>
2019-12-12 21:26:31 +0100

    patchup for changes for adc

--- w_proto/src/hal_bsp.c
+++ w_proto/src/hal_bsp.c
@@ -614,25 +614,22 @@ bool hal_bsp_adc_define(int pin, int chan) {
 // Should read the factory calibrated vref from the eerom at 0x1FF8 00F8/9
 #define ADC_VREF_BANDGAP                            1224    // vRef in mV for ADC
 
-int hal_bsp_adc_readmV(int channel) {
+
+int hal_bsp_adc_read(int channel) {
     ADC_HandleTypeDef* adch = &_adc1.adcHandle;
+    ADC_ChannelConfTypeDef adcConf = { 0 };
     int adcData = 0;
 
-
+    adcConf.Channel = channel;
+    adcConf.Rank = ADC_REGULAR_RANK_1;
+    adcConf.SamplingTime = ADC_SAMPLETIME_192CYCLES;
+    HAL_ADC_ConfigChannel( adch, &adcConf );
     // Start ADC Software Conversion
     HAL_ADC_Start(adch);
 
     HAL_ADC_PollForConversion(adch, HAL_MAX_DELAY);
 
     adcData = HAL_ADC_GetValue(adch);
-
-    int ref_voltage = ( uint32_t )ADC_VREF_BANDGAP * ( uint32_t )ADC_MAX_VALUE;
-    // We don't use the VREF from calibValues here.
-    // calculate the Voltage in millivolt
-    if (adcData>0) {
-        adcData = ref_voltage / ( uint32_t )adcData;
-    }
-//    adcData = (adcData*ADC_VREF_BANDGAP) / ADC_MAX_VALUE;
     return (uint16_t)adcData;
 }
 
@@ -670,7 +667,7 @@ bool hal_bsp_adc_define(int pin, int chan) {
     return false;
 }
 
-int hal_bsp_adc_readmV(int channel) {
+int hal_bsp_adc_read(int channel) {
     return 0;
 }
 
